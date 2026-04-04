e4c7d1bcda2e82f44b0a64f34aac7d77aff67e96
sreynetmon <monsreynet2409@gmail.com>
2026-04-04 09:58:35 +0700

    habit controller

---
+++ src/main/java/com/kshrd/pp_group_02_spring_mini_project/controller/HabitController.java
@@ -0,0 +1,15 @@
+package com.kshrd.pp_group_02_spring_mini_project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/habit")
+@RequiredArgsConstructor
+public class HabitController {
+
+    @GetMapping
+    public
+}
